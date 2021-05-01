4a2f89f9c675f92d2b0a422dab235a664fa4770e
codwerk <louis_widdy@live.fr>
2021-05-01 14:24:35 +0200

    end integration style

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,16 +4,16 @@ const randomMeal = document.getElementById('random-meal');
 
 let search = '';
 
-const fetchSearch = async() => {
-   meals = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+const fetchSearch = async(url) => {
+   meals = await fetch(`https://www.themealdb.com/api/json/v1/1/${url}`)
       .then( data => data.json())
       .then( data => data.meals)
       console.log(meals);
 };
 
-
+// Search
 const searchDisplay = async() => {
-  await fetchSearch();
+  await fetchSearch(search);
 
   if(meals == null) {
     results.innerHTML = `<div class="component-app__result-is-null"><span>Aucun resultat</span></div>`;
@@ -29,7 +29,12 @@ const searchDisplay = async() => {
             <picture>
           </div>
           <div class="component-app__wrap-texts">
-            <h3 class="component-app__wrap-title">Country : ${meal.strArea}</h3>  
+            <h3 class="component-app__wrap-country">${meal.strArea}</h3>
+            <p class="component-app__wrap-info">
+              <span class="component-app__wrap-name">${meal.strMeal}</span> 
+              <span class="component-app__wrap-categorie">${meal.strCategory}</span> 
+            </p>
+             
           </div>
         </div>
         `
@@ -37,10 +42,37 @@ const searchDisplay = async() => {
     );
 }
 
-searchInput.addEventListener('input', (e) => {
+// random meal
+
+const randomMealDisplay = async() => {
+
+  await fetchSearch('random.php');
+
+  results.innerHTML = (
+    meals.map( meal => (
+      `
+      <div class="component-app__random-wrapper-meal">
+        <div class="component-app__random-wrap-picture">
+          <picture>
+              <img src="${meal.strMealThumb}" alt="">
+          <picture>
+        </div>
+        <div class="component-app__random-wrap-texts">
+          <h3 class="component-app__random-wrap-title">${meal.strArea}</h3>
+
+        </div>
+      </div>
+      `
+    )).join('')
+  );
   
-  search = e.target.value;
+}
+
+searchInput.addEventListener('input', (e) => {
+
+  search = `search.php?s=${e.target.value}`;
   searchDisplay();
 })
 
-
+randomMeal.addEventListener('click', randomMealDisplay);
+randomMealDisplay();
